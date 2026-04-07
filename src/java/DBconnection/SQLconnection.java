c22a63444ad3dfc75b0380b45f9f797e3519f70e
karthinarine03 <karthinarine03@gmail.com>
2026-04-07 17:48:26 +0530

    Final DB connection fix

--- src/java/DBconnection/SQLconnection.java
+++ src/java/DBconnection/SQLconnection.java
@@ -1,34 +1,37 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package DBconnection;
 
-/**
- *
- * @author JAVA-JP
- */
 import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class SQLconnection {
-    
-static Connection con;
 
-    
-    /**
-     *
-     * @return
-     */
+    static Connection con;
+
     public static Connection getconnection() {
         try {
-            Class.forName("com.mysql.jdbc.Driver");
-            //to load driver
-            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/prea", "root", "root");
+            // Load MySQL Driver
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            // Get Railway environment variables
+            String host = System.getenv("MYSQLHOST");
+            String port = System.getenv("MYSQLPORT");
+            String database = System.getenv("MYSQLDATABASE");
+            String user = System.getenv("MYSQLUSER");
+            String password = System.getenv("MYSQLPASSWORD");
+
+            // Create connection URL
+            String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false&allowPublicKeyRetrieval=true";
+
+            // Connect to database
+            con = DriverManager.getConnection(url, user, password);
+
+            System.out.println("Database Connected Successfully!");
+
         } catch (Exception e) {
+            System.out.println("Database Connection Failed!");
+            e.printStackTrace(); // VERY IMPORTANT for debugging
         }
+
         return con;
     }
 }
-
